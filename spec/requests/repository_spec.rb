55f7d1eaef24fa1371a2207d9d5f51fd634f8440
julio cabrera <juliocabrera820@gmail.com>
2021-01-29 15:57:03 -0600

    test: request

---
+++ spec/requests/repository_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Repository', type: :request do
+  describe 'GET /users/:user_id/repositories' do
+    let(:user) { FactoryBot.create(:user) }
+    it 'returns unauthorized status due to authorization header is missing' do
+      get '/api/v1/users/1/repositories'
+      expect(response).to have_http_status(:unauthorized)
+    end
+    let(:user) { FactoryBot.create(:user) }
+    it 'returns a new repository' do
+      token = AuthenticationService.encode(user)
+      post '/api/v1/users/1/repositories', params: { name: 'demo', description: 'demo jwt', visible: true },
+                                           headers: { Authorization: "Bearer #{token}" }
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to eq(
+        {
+          'id' => 1,
+          'name' => 'demo',
+          'description' => 'demo jwt',
+          'visible' => true
+        }
+      )
+    end
+  end
+end
