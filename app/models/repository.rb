bf9f87c4e3e56cd6a2b017e15082d9eb17296435
julio cabrera <juliocabrera820@gmail.com>
2021-04-24 23:42:57 -0500

    remove unneeded validation

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Repository < ApplicationRecord
-  validates :name, :description, :visible, presence: true
+  validates :name, :description, presence: true
   belongs_to :user
 end
