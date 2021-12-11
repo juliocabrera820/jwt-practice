93ba7b5a66d00a4c414f230a3a2202a94ada3c51
juliocabrera820 <juliocabrera820@gmail.com>
2021-12-11 14:23:10 -0600

    move let from block

--- spec/requests/repository_spec.rb
+++ spec/requests/repository_spec.rb
@@ -4,14 +4,13 @@ require 'rails_helper'
 
 RSpec.describe 'Repository', type: :request do
   describe 'GET /users/:user_id/repositories' do
-    let(:user) { FactoryBot.create(:user) }
     it 'returns unauthorized status due to authorization header is missing' do
       get '/api/v1/users/1/repositories'
       expect(response).to have_http_status(:unauthorized)
     end
   end
+  let(:user) { FactoryBot.create(:user) }
   describe 'POST /users/:user_id/repositories' do
-    let(:user) { FactoryBot.create(:user) }
     it 'returns a new repository' do
       token = AuthenticationService.encode(user)
       post "/api/v1/users/#{user.id}/repositories", params: { name: 'demo', description: 'demo jwt', visible: true },
